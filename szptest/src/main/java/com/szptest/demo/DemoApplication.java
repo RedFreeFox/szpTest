8ed51cc5a7158ad8349d909f3bb5acdbd9bf1b86
szp <1412420939@qq.com>
2020-04-16 16:10:37 +0800

    去掉不需要的jar

--- szptest/src/main/java/com/szptest/demo/DemoApplication.java
+++ szptest/src/main/java/com/szptest/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.szptest.demo;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
